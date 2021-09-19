dffcff16cfe471743b4230995f51aa027ac44323
Sharon Lee <sharonshlee@icloud.com>
2021-09-19 11:00:38 -0400

    edit tables schema and seeds

--- db/schema/01_create_tables.sql
+++ db/schema/01_create_tables.sql
@@ -1,32 +1,24 @@
-DROP TABLE IF EXISTS clients CASCADE;
 DROP TABLE IF EXISTS dishes CASCADE;
 DROP TABLE IF EXISTS orders CASCADE;
 
-CREATE TABLE clients (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  phone_number INTEGER NOT NULL,
-  street VARCHAR(255) NOT NULL,
-  city VARCHAR(255) NOT NULL,
-  post_code VARCHAR(255) NOT NULL,
-  province VARCHAR(255) NOT NULL,
-  country VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
 
 CREATE TABLE dishes (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
+  category VARCHAR(255),
   description TEXT,
-  cooking_time INTEGER,
   price INTEGER,
-  img_url VARCHAR
+  img_url VARCHAR(255),
+  cooking_time INTEGER
 );
 
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   quantity INTEGER NOT NULL,
   dish_id INTEGER REFERENCES dishes(id) ON DELETE CASCADE,
-  clients_id INTEGER REFERENCES clients(id) ON DELETE CASCADE,
-  order_type VARCHAR(255)
+  order_type TEXT,
+  placed_at TIMESTAMP,
+  finished_at TIMESTAMP,
+  customer_name VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(255) NOT NULL
 );
--- db/seeds/01_insert_tables.sql
+++ db/seeds/01_insert_tables.sql
@@ -1,17 +1,11 @@
-INSERT INTO clients (name, phone_number, street, city, post_code, province, country, password)
+INSERT INTO dishes (name, category, description, price, img_url, cooking_time)
 VALUES
-('Sharon Lee', 1234567891, '123 fake street', 'toronto', 'A1B 2C3', 'ON', 'canada', 'password'),
-('Sam Fay', 1234567892, '124 fake street', 'ottawa', 'A1B 2C4', 'ON', 'canada', 'password'),
-('Lewis Lee', 1234567893, '125 fake street', 'toronto', 'A1B 2C5', 'ON', 'canada', 'password');
+('Manakish', 'Bakery', 'Delicious and crispy homemade dough is topped with a zaatar topping or a blend of cheeses', 15, 'https://www.carolinescooking.com/wp-content/uploads/2021/02/Manakish-Lebanese-zaatar-flatbread-photo.jpg', 15),
+('Nasi Kerabu', 'Rice', 'Delicious blue-colored rice is eaten with dried fish or fried chicken, crackers, pickles and other salads.', 17, 'https://asianinspirations.com.au/wp-content/uploads/2019/05/R02216-Ayam-Percik-(BBQ-Spiced-Chicken)-&-Nasi-Kerabu-(Herb-Rice-Salad)-(CSW-2019).jpg', 30),
+('Bibimbap', 'Rice', 'Delcious dish of rice with cooked vegetables, usually meat, and often a raw or fried egg', 13, 'https://www.koreanbapsang.com/wp-content/uploads/2015/01/DSC_0159-e1537669822110.jpg', 20);
 
-INSERT INTO dishes (name, cooking_time, description, price, img_url)
+INSERT INTO orders (quantity, dish_id, order_type, placed_at, finished_at, customer_name, phone_number)
 VALUES
-('Manakish', 5, 'Delicious and crispy homemade dough is topped with a zaatar topping or a blend of cheeses', 15, 'https://www.carolinescooking.com/wp-content/uploads/2021/02/Manakish-Lebanese-zaatar-flatbread-photo.jpg'),
-('Nasi Kerabu', 7, 'Delicious blue-colored rice is eaten with dried fish or fried chicken, crackers, pickles and other salads.', 17, 'https://asianinspirations.com.au/wp-content/uploads/2019/05/R02216-Ayam-Percik-(BBQ-Spiced-Chicken)-&-Nasi-Kerabu-(Herb-Rice-Salad)-(CSW-2019).jpg'),
-('Bibimbap', 3, 'Delcious dish of rice with cooked vegetables, usually meat, and often a raw or fried egg', 13, 'https://www.koreanbapsang.com/wp-content/uploads/2015/01/DSC_0159-e1537669822110.jpg');
-
-INSERT INTO orders (quantity, dish_id, clients_id, order_type)
-VALUES
-(3, 1,2, 'business'),
-(2, 2, 2, 'home'),
-(1, 3, 2, 'business');
+(3, 1, 'business', '2021-06-22 19:10:25-07', '2021-06-22 20:10:25-07', 'Lewis', '+1263845504'),
+(1, 2, 'home', '2021-06-22 19:10:25-07', '2021-06-22 21:10:25-07', 'Sam', '+54353455435'),
+(4, 3, 'business', '2021-06-22 19:10:25-07', '2021-06-22 22:10:25-07', 'Sharon', '+126384535434');
