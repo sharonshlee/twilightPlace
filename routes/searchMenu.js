f6d1464eafb78c6f9e47992ecc5d06843c575d8f
Lewis Lee <lewisforbusiness@gmail.com>
2021-09-23 12:28:35 -0400

    Fixed the edit order quantity issue. Also works for searchMenu

--- routes/searchMenu.js
+++ routes/searchMenu.js
@@ -4,7 +4,10 @@ const router = express.Router();
 module.exports = (db) => {
 
   router.get('/', (req, res) => {
-    const templateVars = {dishes: req.session.searchResults};
+    const templateVars = {dishes: req.session.searchResults, cartItems:[]};
+    if (req.session.cartItems) {
+      templateVars.cartItems = req.session.cartItems.quantity;
+    }
     res.render('menu', templateVars);
   })
 
@@ -17,11 +20,6 @@ module.exports = (db) => {
       WHERE category LIKE $1
       OR name LIKE $1;
       `, [`%${removeSpaces}%`])
-    // db.query(`
-    // SELECT * FROM dishes
-    // WHERE category LIKE '%${removeSpaces}%'
-    // OR name LIKE '%${removeSpaces}%';
-    // `)
     .then(result => {
       console.log('result is: ', result.rows);
       req.session.searchResults = result.rows;
