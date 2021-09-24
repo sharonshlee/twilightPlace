0d40658c009e62130a78ac7505dde49c2be86fd7
Sharon Lee <sharonshlee@icloud.com>
2021-09-23 21:28:45 -0400

    refactor connection to db

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const cookieSession = require("cookie-session");
 const app = express();
 
 // PG database client/connection setup
-const { Pool } = require("pg");
-const dbParams = require("./lib/db.js");
-const db = new Pool(dbParams);
-db.connect();
+const db = require("./db/pool");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
